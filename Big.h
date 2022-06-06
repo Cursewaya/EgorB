805fce6ca55135b69c90ac4bd7d2ca282ed5801c
Cursewaya <105514438+Cursewaya@users.noreply.github.com>
2022-06-07 01:37:15 +0300

    Update Big.h

--- Big.h
+++ Big.h
@@ -7,15 +7,33 @@
 #include <string>
 #include <windows.h>
 #include <math.h>
-
+#include <time.h>
 using namespace std;
-class Big
+class Num
 {
+protected:
 	vector<int> bef;
 public:
-	Big(){}
+	Num() {};
+	Num(string s) {};
+	Num(long long s) {};
+	Num(vector<int>& b) {};
+	Num(const Num& b) {};
+	~Num() {};
+	Num operator +(Num b) {};
+	Num operator /(Num b) {};
+	Num operator %(Num b) {};
+	Num operator -(Num b) {};
+	Num operator *(Num b) {};
+};
+
+class Big:  public Num
+{
+public:
+	Big() {}
 	Big(string s)
 	{
+		bef.erase(bef.begin(), bef.end());
 		int check = 0;
 		if (s[0] == '-')
 		{
@@ -27,23 +45,6 @@ public:
 			bef.push_back(int(s[i]) - 48);
 		}
 	}
-	Big(long long s)
-	{
-		bef.push_back(s % 10);
-		s = s / 10;
-		while (s > 0)
-		{
-			bef.push_back(s % 10);
-			s = s / 10;
-		}
-	}
-	Big(vector<int>&b)
-	{
-		for (int i = 0; i <b.size(); ++i)
-		{
-			bef.push_back( b[i]);
-		}
-	}
 	Big(const Big& b)
 	{
 		bef.erase(bef.begin(), bef.end());
@@ -53,18 +54,54 @@ public:
 			bef[i] = b.bef[i];
 		}
 	}
+	Big(vector<int>& b)
+	{
+		bef.erase(bef.begin(), bef.end());
+		for (int i = 0; i < b.size(); ++i)
+		{
+			bef.push_back(b[i]);
+		}
+	}
+	Big(long long s) 
+	{
+		bef.erase(bef.begin(), bef.end());
+		int znak = 1;
+		if (s < 0)
+		{
+			znak = -1;
+			s = s * -1;
+		}
+		if (s == 0)
+		{
+			bef.push_back(0);
+			return;
+		}
+
+		while (s > 0)
+		{
+			bef.push_back(s % 10);
+			s = s / 10;
+		}
+		reverse(bef.begin(), bef.end());
+		bef[0] *= znak;
+	}
+	
+	
 	~Big() {}
+	void cut();
 	Big operator +(Big b);
-	Big operator /(Big b);
-	Big operator %(Big b);
-	Big operator -(Big b);
-	Big operator *(Big b);
+	Big operator /(Big b) ;
+	Big operator %(Big b) ;
+	Big operator -(Big b) ;
+	Big operator *(Big b) ;
 
-	friend Big operator *(Big a,long long b);
-	friend Big cut(Big);
-	friend bool operator >(Big a,Big b);
-
-	/*friend void operator =(Big a,Big b);*/
+	
+		
+	friend Big operator *(Big a, long long b);
+	friend bool operator >(Big a, Big b);
+	friend bool operator <(Big a, Big b);
+	friend bool operator ==(Big a, Big b);
+	friend bool operator !=(Big a, Big b);
 	friend ostream& operator<<(ostream& os, const Big& s);
 	friend istream& operator>> (istream& is, Big& s);
 
